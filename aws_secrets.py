058a5a298a50770646f14deeecbbb42eb6086b36
galevy88 <gallevy88@gmail.com>
2023-12-27 00:55:11 +0200

    adding sk to aws

---
+++ aws_secrets.py
@@ -0,0 +1,15 @@
+import boto3
+import json
+
+def get_secret(secret_name):
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager')
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except Exception as e:
+        raise e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+            return json.loads(secret)
